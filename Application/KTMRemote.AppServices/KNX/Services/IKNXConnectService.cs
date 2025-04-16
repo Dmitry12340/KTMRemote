19269cd08d711ff2202f711d4fceded2aefb6bbd
Dmitry <diman_chelyad@mail.ru>
2025-04-16 20:59:35 +0300

    Добавил описание сервиса и его компонентов

--- Application/KTMRemote.AppServices/KNX/Services/IKNXConnectService.cs
+++ Application/KTMRemote.AppServices/KNX/Services/IKNXConnectService.cs
@@ -2,10 +2,26 @@
 using KTMRemote.Contracts.KNXDto;
 
 namespace KTMRemote.AppServices.KNX.Services;
+/// <summary>
+/// Сервис для поключения к интерфейсу knx
+/// </summary>
 public interface IKNXConnectService
 {
+    /// <summary>
+    /// Свойство, содержащее подключение к интерфейсу knx.
+    /// </summary>
     KnxBus Bus { get; }
+
+    /// <summary>
+    /// Подключение к интерфейсу knx
+    /// </summary>
+    /// <param name="connectDto">Транспортная модель.</param>
+    /// <param name="cancellation">Токен отмены.</param>
     Task ConnectAsync(KNXConnectDto connectDto, CancellationToken cancellation);
-    Task CreateIpTunnelingAsync(KNXConnectDto connectDto, CancellationToken cancellation);
+
+    /// <summary>
+    /// Отключение от интерфейса knx
+    /// </summary>
+    /// <param name="cancelToken">Токен отмены.</param>
     Task DisconnectAsync(CancellationToken cancelToken);
 }
