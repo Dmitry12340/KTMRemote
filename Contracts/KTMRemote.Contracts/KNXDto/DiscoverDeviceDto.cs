3b11c29d2f1a3e6bbc96359b458afcc1fab941d9
Dmitry <diman_chelyad@mail.ru>
2025-04-16 16:51:26 +0300

    Добавил DiscoverDeviceDto для обмена между слоями приложения

---
+++ Contracts/KTMRemote.Contracts/KNXDto/DiscoverDeviceDto.cs
@@ -0,0 +1,9 @@
+﻿namespace KTMRemote.Contracts.KNXDto;
+public sealed class DiscoverDeviceDto
+{
+    public string Name;
+    public string IndividualAddress;
+    public bool ProgrammingMode;
+    public string Ip;
+    public int Port;
+}
