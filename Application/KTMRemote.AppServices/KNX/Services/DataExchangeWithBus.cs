8c81e8dccb0f4c9350e54b3266f519090ac6d058
Dmitry <diman_chelyad@mail.ru>
2025-04-17 15:27:22 +0300

    Реализовал сервис для обмена сообщениями с интерфейсом knx

---
+++ Application/KTMRemote.AppServices/KNX/Services/DataExchangeWithBus.cs
@@ -0,0 +1,21 @@
+﻿using Knx.Falcon;
+using KTMRemote.Contracts.KNXDto;
+
+namespace KTMRemote.AppServices.KNX.Services;
+
+public class DataExchangeWithBus : IDataExchangeWithBus
+{
+    private readonly IKNXConnectService _knxConnectService;
+    public DataExchangeWithBus(IKNXConnectService knxConnectService)
+    {
+        _knxConnectService = knxConnectService;
+    }
+    public async Task<bool> WriteGroupAsync(KNXMassageDto massageDto, CancellationToken cancellation)
+    {
+        GroupAddress address = massageDto.Address;
+        GroupValue value = new GroupValue(massageDto.Value);
+        MessagePriority priority = (MessagePriority)massageDto.Priority;
+        bool result = await _knxConnectService.Bus.WriteGroupValueAsync(address, value, priority, cancellation);
+        return result;
+    }
+}
