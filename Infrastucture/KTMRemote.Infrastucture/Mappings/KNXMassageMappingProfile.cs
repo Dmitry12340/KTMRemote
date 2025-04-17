2ab45f0a6572e1de8d8f184596ff3244c96ec9ed
Dmitry <diman_chelyad@mail.ru>
2025-04-17 15:29:38 +0300

    Добавил автомапер

---
+++ Infrastucture/KTMRemote.Infrastucture/Mappings/KNXMassageMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using KTMRemote.Contracts.KNXDto;
+//using KTMRemote.MVC.Models;
+
+namespace KTMRemote.Infrastucture.Mappings;
+
+public sealed class KNXMassageMappingProfile : Profile
+{
+    public KNXMassageMappingProfile()
+    {
+        /*CreateMap<KNXGroupModel, KNXMassageDto>()
+            .ForMember(dest => dest.Address, o => o.MapFrom(src => src.address))
+            .ForMember(dest => dest.Value, o => o.MapFrom(src => src.value));*/
+    }
+}
