f5bf7b6eac6ae74b72035367e7c8640b3693d150
Dmitry <diman_chelyad@mail.ru>
2025-04-17 15:26:44 +0300

    Добавил регистрацию автомапера

--- Infrastucture/KTMRemote.ComponentRegistrar/Registrar.cs
+++ Infrastucture/KTMRemote.ComponentRegistrar/Registrar.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using KTMRemote.AppServices.KNX.Services;
+using AutoMapper;
+using KTMRemote.Infrastucture.Mappings;
 
 namespace KTMRemote.ComponentRegistrar;
 public static class Registrar
@@ -8,11 +10,24 @@ public static class Registrar
     public static void AddComponents(IServiceCollection services, IConfiguration configuration)
     {
         RegisterServices(services);
+        RegisterMapper(services, configuration);
     }
 
     private static void RegisterServices(IServiceCollection services)
     {
         services.AddSingleton<IKNXConnectService, KNXConnectService>();
+        services.AddScoped<IDataExchangeWithBus, DataExchangeWithBus>();
         services.AddScoped<IKNXDiscoverIpDevicesService, KNXDiscoverIpDevicesService>();
+    }
+
+    private static void RegisterMapper(IServiceCollection services, IConfiguration configuration)
+    {
+        var mapperConfig = new MapperConfiguration(mc =>
+        {
+            mc.AddProfile(new KNXMassageMappingProfile());
+        });
+
+        IMapper mapper = mapperConfig.CreateMapper();
+        services.AddSingleton(mapper);
     }
 }
